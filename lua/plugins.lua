a787c327171a2a067eaf2bdf545c16b3acd52ba3
Maximilian R <maxi.rostock@outlook.de>
2022-11-23 16:25:17 +0100

    add autocomplete

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,15 +1,38 @@
 packer = require('packer')
 
 packer.init {
-	max_jobs = 16
+  max_jobs = 16
 }
-return packer.startup(
-function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
 
-  use {
-     'nvim-lualine/lualine.nvim',
-     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-end)
+return packer.startup(
+  function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
+    use 'neovim/nvim-lspconfig'
+
+    use {
+      'hrsh7th/vim-vsnip',
+      requires = { 'hrsh7th/vim-vsnip-integ' }
+    }
+
+    use {
+      "hrsh7th/nvim-cmp",
+      requires = {
+        "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
+        'quangnguyen30192/cmp-nvim-ultisnips', 'hrsh7th/cmp-nvim-lua',
+        'octaltree/cmp-look', 'hrsh7th/cmp-path', 'hrsh7th/cmp-calc',
+        'f3fora/cmp-spell', 'hrsh7th/cmp-emoji'
+      }
+    }
+    use {
+      'tzachar/cmp-tabnine',
+      run = './install.sh',
+      requires = 'hrsh7th/nvim-cmp'
+    }
+  end)
