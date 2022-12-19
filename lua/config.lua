69a4a8808f7baac5deb64779e6d14246f4e19fa3
Maximilian <maxi.rostock@outlook.de>
2022-12-19 19:52:34 +0100

    format config, add option

--- lua/config.lua
+++ lua/config.lua
@@ -1,8 +1,16 @@
+-- Set up relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Set up tab width
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.autoindent = true
 vim.opt.smartindent = true
+
+-- Set up case-insensitive search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+--Disable showing e. g. --INSERT-- when inserting since lualine does this in a way that doesn't end up fugly
+vim.opt.showmode = false
