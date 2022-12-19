6264e4c95cba971328fed07531b50f6be74631d6
Maximilian R <maxi.rostock@outlook.de>
2022-12-19 18:24:39 +0100

    add troubles and format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,32 +1,43 @@
 local packer = require('packer')
 
 return packer.startup(
-  function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' }
+  }
+
+  use 'neovim/nvim-lspconfig'
+
+  use {
+    'hrsh7th/vim-vsnip',
+    requires = { 'hrsh7th/vim-vsnip-integ' }
+  }
+
+  use {
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
+      'quangnguyen30192/cmp-nvim-ultisnips', 'hrsh7th/cmp-nvim-lua',
+      'octaltree/cmp-look', 'hrsh7th/cmp-path', 'hrsh7th/cmp-calc',
+      'f3fora/cmp-spell', 'hrsh7th/cmp-emoji'
     }
+  }
+  -- use 'andweeb/presence.nvim'
 
-    use 'neovim/nvim-lspconfig'
-
-    use {
-      'hrsh7th/vim-vsnip',
-      requires = { 'hrsh7th/vim-vsnip-integ' }
-    }
-
-    use {
-      "hrsh7th/nvim-cmp",
-      requires = {
-        "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
-        'quangnguyen30192/cmp-nvim-ultisnips', 'hrsh7th/cmp-nvim-lua',
-        'octaltree/cmp-look', 'hrsh7th/cmp-path', 'hrsh7th/cmp-calc',
-        'f3fora/cmp-spell', 'hrsh7th/cmp-emoji'
+  use 'noahfrederick/vim-skeleton'
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
       }
-    }
-    -- use 'andweeb/presence.nvim'
-
-    use 'noahfrederick/vim-skeleton'
-  end)
+    end
+  }
+end)
