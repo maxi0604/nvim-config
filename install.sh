fd2308579b39739f5246c3b25da0b934cedb343c
Maximilian R <maxi.rostock@outlook.de>
2022-12-21 00:00:39 +0100

    fix install script

--- install.sh
+++ install.sh
@@ -6,11 +6,14 @@ else
   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
 
-if [ -n $TERMUX_VERSION ]; then
+if [ -n "$TERMUX_VERSION" ]; then
   echo "termux detected. running pkg install"
   yes | pkg install rust-analyzer lua-language-server
-elif [ -x "$(command -v foo)" ] then
-  sudo pacman -Syu rust-analyzer lua-language-server
+elif [ -x "$(command -v pacman)" ]; then
+  echo "pacman detected. please grant permission for installing rust-analyzer and lua-language-server."
+  sudo pacman -Syu --noconfirm rust-analyzer lua-language-server
+else
+  echo "ERROR: your package manager is not supported. please install rust-analyzer and lua-language-server"
 fi
 
 echo "installing python through pip"
